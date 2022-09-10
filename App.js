545621861512fc553313e62592aaa6b00c157ae4
Prince Verma <princeverma@Princes-MacBook-Pro.local>
2022-09-11 02:34:27 +0530

    change app func to arrow func and remove boiler plate

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <>
       <StatusBar style="dark" />
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
     </>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
