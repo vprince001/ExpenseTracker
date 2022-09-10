839a936f114e08812c2165b0a7467cec93339288
Prince Verma <princeverma@Princes-MacBook-Pro.local>
2022-09-11 02:24:43 +0530

    initial commit - add expo StatusBar with dark mode

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </>
   );
 }
 
