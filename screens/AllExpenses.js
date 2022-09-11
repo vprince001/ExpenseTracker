002b49821390b230e349762504dfc68cbcf799e5
Prince Verma <princeverma@Princes-MacBook-Pro.local>
2022-09-11 12:25:09 +0530

    extract expenses dummy data to util data file

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,42 +1,6 @@
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-const expenses = [
-  {
-    id: 1,
-    description: 'first expense',
-    amount: 10,
-    category: 'first category',
-    date: new Date('2022-09-07').toISOString(),
-  },
-  {
-    id: 2,
-    description: 'second expense',
-    amount: 20,
-    category: 'second category',
-    date: new Date('2022-09-07').toISOString(),
-  },
-  {
-    id: 3,
-    description: 'third expense',
-    amount: 30,
-    category: 'third category',
-    date: new Date('2022-09-07').toISOString(),
-  },
-  {
-    id: 4,
-    description: 'fourth expense',
-    amount: 40,
-    category: 'fourth category',
-    date: new Date('2022-09-07').toISOString(),
-  },
-  {
-    id: 5,
-    description: 'fifth expense',
-    amount: 50,
-    category: 'fifth category',
-    date: new Date('2022-09-07').toISOString(),
-  },
-];
+import { EXPENSES } from '../util/data';
 
 const ExpenseItem = ({ id, description, amount, category, date }) => {
   return (
@@ -56,9 +20,9 @@ const renderExpenseItem = (itemData) => {
 
 const AllExpenses = () => {
   return (
-    <View style={ styles.container}>
+    <View style={styles.container}>
       <FlatList
-        data={expenses}
+        data={EXPENSES}
         renderItem={renderExpenseItem}
         keyExtractor={(item) => item.id}
       />
---
+++ util/data.js
@@ -0,0 +1,37 @@
+export const EXPENSES = [
+  {
+    id: 1,
+    description: 'first expense',
+    amount: 10,
+    category: 'first category',
+    date: new Date('2022-09-07').toISOString(),
+  },
+  {
+    id: 2,
+    description: 'second expense',
+    amount: 20,
+    category: 'second category',
+    date: new Date('2022-09-07').toISOString(),
+  },
+  {
+    id: 3,
+    description: 'third expense',
+    amount: 30,
+    category: 'third category',
+    date: new Date('2022-09-07').toISOString(),
+  },
+  {
+    id: 4,
+    description: 'fourth expense',
+    amount: 40,
+    category: 'fourth category',
+    date: new Date('2022-09-07').toISOString(),
+  },
+  {
+    id: 5,
+    description: 'fifth expense',
+    amount: 50,
+    category: 'fifth category',
+    date: new Date('2022-09-07').toISOString(),
+  },
+];
