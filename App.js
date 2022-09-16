438a740f5797c079e0b409cd5b57e36f39087cc6
Prince Verma <princeverma@192.168.1.4>
2022-09-17 02:08:00 +0530

    add bottom tabs

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import AllExpenses from './screens/AllExpenses'
 import ManageExpense from './screens/ManageExpense'
@@ -11,48 +12,58 @@ import { ScreenNames } from './constants/screens'
 import IconButton from './components/UI/IconButton'
 import { GlobalStyles } from './constants/styles'
 
-const Stack = createNativeStackNavigator()
+const ExpensesStack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const ExpensesStackScreen = () => {
+  return (
+    <ExpensesContextProvider>
+      <ExpensesStack.Navigator>
+        <ExpensesStack.Screen
+          name={ScreenNames.allExpenseScreen}
+          component={AllExpenses}
+          options={({ navigation }) => {
+            return {
+              title: 'Expenses',
+              headerTitleAlign: 'center',
+              headerRight: () => (
+                <IconButton
+                  icon={'add'}
+                  size={36}
+                  color={GlobalStyles.colors.primary300}
+                  onPress={() => {
+                    navigation.navigate(ScreenNames.manageExpenseScreen)
+                  }}
+                />
+              ),
+            }
+          }}
+        />
+        <ExpensesStack.Screen
+          name={ScreenNames.manageExpenseScreen}
+          component={ManageExpense}
+          options={{
+            presentation: 'modal',
+            headerTitleAlign: 'center',
+          }}
+        />
+      </ExpensesStack.Navigator>
+    </ExpensesContextProvider>
+  )
+}
 
 const App = () => {
   return (
     <>
       <StatusBar style="dark" />
-      <ExpensesContextProvider>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name={ScreenNames.allExpenseScreen}
-              component={AllExpenses}
-              options={({ navigation }) => {
-                return {
-                  title: 'Expenses',
-                  headerTitleAlign: 'center',
-                  headerRight: () => {
-                    return (
-                      <IconButton
-                        icon={'add'}
-                        size={36}
-                        color={GlobalStyles.colors.primary300}
-                        onPress={() => {
-                          navigation.navigate(ScreenNames.manageExpenseScreen)
-                        }}
-                      />
-                    )
-                  },
-                }
-              }}
+          <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen
+              name={'AllExpensesBottomTab'}
+              component={ExpensesStackScreen}
             />
-            <Stack.Screen
-              name={ScreenNames.manageExpenseScreen}
-              component={ManageExpense}
-              options={{
-                presentation: 'modal',
-                headerTitleAlign: 'center',
-              }}
-            />
-          </Stack.Navigator>
+          </Tab.Navigator>
         </NavigationContainer>
-      </ExpensesContextProvider>
     </>
   )
 }
