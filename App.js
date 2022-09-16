e84c3ac42ffa5679431e47d1e3fcde8203c44b3f
Prince Verma <princeverma@192.168.1.4>
2022-09-17 00:41:49 +0530

    remove bottom tabs and add button from all expense screen

--- App.js
+++ App.js
@@ -1,52 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
-import { Ionicons } from '@expo/vector-icons'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import AllExpenses from './screens/AllExpenses'
 import ManageExpense from './screens/ManageExpense'
 
-import { GlobalStyles } from './constants/styles'
-import IconButton from './components/UI/IconButton'
 import ExpensesContextProvider from './screens/store/expenses-context'
 
 const Stack = createNativeStackNavigator()
-const BottomTabs = createBottomTabNavigator()
-
-const ExpensesOverview = () => {
-  return (
-    <BottomTabs.Navigator
-      screenOptions={({ navigation }) => ({
-        headerStyle: { backgroundColor: GlobalStyles.colors.clean },
-        tabBarStyle: { backgroundColor: GlobalStyles.colors.clean },
-        tabBarActiveTintColor: GlobalStyles.colors.primary300,
-        headerRight: () => (
-          <IconButton
-            icon="add"
-            size={36}
-            onPress={() => navigation.navigate('ManageExpense')}
-            color={GlobalStyles.colors.primary300}
-          />
-        ),
-      })}
-    >
-      <BottomTabs.Screen
-        name="AllExpenses"
-        component={AllExpenses}
-        options={{
-          title: 'All Expenses',
-          tabBarShowLabel: false,
-          headerTitleAlign: 'center',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="list" color={color} size={size} />
-          ),
-        }}
-      />
-    </BottomTabs.Navigator>
-  )
-}
 
 const App = () => {
   return (
@@ -56,14 +18,20 @@ const App = () => {
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen
-              name="ExpensesOverview"
-              component={ExpensesOverview}
-              options={{ headerShown: false }}
+              name="AllExpenses"
+              component={AllExpenses}
+              options={{
+                title: 'Expenses',
+                headerTitleAlign: 'center',
+              }}
             />
             <Stack.Screen
               name="ManageExpense"
               component={ManageExpense}
-              options={{ presentation: 'modal', headerTitleAlign: 'center' }}
+              options={{
+                presentation: 'modal',
+                headerTitleAlign: 'center',
+              }}
             />
           </Stack.Navigator>
         </NavigationContainer>
